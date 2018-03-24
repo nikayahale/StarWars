1db32205cdbd65759e75d7bf82a82a2584d15bf8
Dagzy <quincye.williams@gmail.com>
2018-03-23 20:35:37 -0400

    QW refactor

--- starWars.js
+++ starWars.js
@@ -1,24 +1,8 @@
 const baseURL = 'https:swapi.co/api';
 let url;
-
-var starWarsPlanetsList = document.querySelector('ul');
-const climate = document.querySelector('.climate')
-const view = document.querySelector('.view')
-const population = document.querySelector('.population')
-// const submitBtn = document.querySelector('.submit')
-const hotBtn = document.querySelector('.hot')
-const tempBtn = document.querySelector('.temperate')
-const coldBtn = document.querySelector('.cold')
-// const mountBtn = document.querySelector('.mountains')
-// const beachBtn = document.querySelector('.beach')
-// const cityBtn = document.querySelector('.city')
-// const highBtn = document.querySelector('.high')
-// const modBtn = document.querySelector('.moderate')
-// const lowBtn = document.querySelector('.low')
 var planetClimate;
-
-
-
+var singleClimate = [];
+var multiClimate = [];
 
 fetch('https://swapi.co/api/planets')
     .then(function (response) {
@@ -26,63 +10,35 @@ fetch('https://swapi.co/api/planets')
     }).then(function (json) {
         // console.log(json)
         var climate = json.results
-        console.log(climate)
+        // console.log(climate)
+        initializeClimate(climate)
+        console.log(singleClimate)
         return planetClimate = climate
     })
-// searchForm.addEventListener('submit', pickPlanet);
-hotBtn.addEventListener('click', hotButton);
-tempBtn.addEventListener('click', tempButton);
-coldBtn.addEventListener('click', coldButton);
-// mountBtn.addEventListener('click', landscape)
-// beachBtn.addEventListener('click', landscape)
-// cityBtn.addEventListener('click', landscape)
-// highBtn.addEventListener('click', people)
-// modBtn.addEventListener('click', people)
-// lowBtn.addEventListener('click', people)
-for (c of climate) {
-    if (c.climate == 'temperate') {
-        console.log(c.climate)
-        // let listItem = document.createElement('li')
-        // listItem.innerHTML = c.climate;
-        // starWarsPlanetsList.appendChild(listItem)
+setTimeout(() => console.log(planetClimate), 3000)
+
+function getClimate(e) {
+    for (let i = 0; i < singleClimate.length; i++) {
+        const element = singleClimate[i];
+        if (e.target.id === element.climate) {
+            console.log('blaggity')
+        }
+        // console.log(element)
     }
 }
 
-function hotButton(e) {
-    e.preventDefault()
-    console.log(planetClimate)
-    for (let i = 0; i < planetClimate.length; i++) {
-        const element = planetClimate[i];
-        if (element.climate == "temperate, tropical" || element.climate == "temperate, arid"){
-            let listItem = document.createElement('li')
-            listItem.innerHTML = element.name;
-            starWarsPlanetsList.appendChild(listItem)
-        }
-    }
-}
 
-function tempButton(e) {
-    e.preventDefault()
-    console.log(planetClimate)
-    for (let i = 0; i < planetClimate.length; i++) {
-        const element = planetClimate[i];
-        if (element.climate == "temperate" || element.climate == "murky"){
-            let listItem = document.createElement('li')
-            listItem.innerHTML = element.name;
-            starWarsPlanetsList.appendChild(listItem)
-        }
-    }
-}
 
-function coldButton(e) {
-    e.preventDefault()
-    console.log(planetClimate)
-    for (let i = 0; i < planetClimate.length; i++) {
-        const element = planetClimate[i];
-        if (element.climate == "frozen"){
-            let listItem = document.createElement('li')
-            listItem.innerHTML = element.name;
-            starWarsPlanetsList.appendChild(listItem)
+function initializeClimate(climateArr) {
+    for (let i = 0; i < climateArr.length; i++) {
+        const element = climateArr[i];
+        if (element.climate.includes(',')) {
+            multiClimate.push(element)
+            stringArray = element.climate.split(',')
+        } else {
+            singleClimate.push(element)
         }
+
     }
+
 }
